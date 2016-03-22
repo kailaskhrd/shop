8ef10a9e6e0dfb5bb04794659f69a1d3e50bbd55
kailaskharad <kailas.kharad@infibeam.net>
2016-03-22 19:20:43 +0530

    resolved logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 	get '/login' => 'sessions#new', :as => 'login'
-	get '/logout' => 'sessions#destroy', :as => 'logout'
+	delete '/logout' => 'sessions#destroy', :as => 'logout'
 	get '/signup' => 'users#new', :as => 'signup'
 	resources :sessions, :only => [:new, :create, :destroy]
 
